5a96cc5df237b2717e87d22ef4d976c0c725c8d9
abrahamshenghur <ashenghur@gmail.com>
2020-12-20 20:17:50 -0800

    Delete storyboard

--- Practice_SearchBar/AppDelegate.swift
+++ Practice_SearchBar/AppDelegate.swift
@@ -16,6 +16,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let navController = UINavigationController(rootViewController: ViewController())
+        navController.navigationBar.prefersLargeTitles = true
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = navController
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
